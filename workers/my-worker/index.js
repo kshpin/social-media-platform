cae479d025f0f499bdccb91964468b9f3b58c20c
kirill24855 <kirill.shpin@gmail.com>
2021-12-18 01:19:49 -0800

    restructured to reflect order of property importance

--- workers/my-worker/index.js
+++ workers/my-worker/index.js
@@ -1,130 +1,157 @@
-function constructResponse(message, status, request) {
-    return new Response(JSON.stringify({ message }), {
-        status,
-        headers: {
-            "content-type": "application/json",
-            "Access-Control-Allow-Origin": request.headers.get("origin"),
-        },
-        credentials: "include",
-    });
-}
+async function router(event, routerConfig) {
+    let endpoint = new URL(event.request.url).pathname;
+    if (routerConfig.hasOwnProperty(endpoint)) {
+        let endpointHandlerMethods = routerConfig[endpoint];
 
-async function handleGet(request) {
-    if (!/.dev\/posts/.test(request.url)) {
-        // there has to be a better way to check what the endpoint is
-        // but I don't know it :/
+        let method = event.request.method.toUpperCase();
+        if (endpointHandlerMethods.hasOwnProperty(method)) {
+            let handler = endpointHandlerMethods[method];
 
-        // not found
-        return constructResponse("Only GET /posts is supported", 404, request);
-    }
-
-    // the storage also includes other information, prefix limits us to posts only
-    let list = await MY_KV.list({ prefix: "post:" });
-    let posts = [];
-    for (key of list.keys) {
-        // parsing every single one is quite inefficient
-        // can technically treat it as a string always, and append commas and such...
-        // but this is much easier to understand as code
-        posts.push(JSON.parse(await MY_KV.get(key.name)));
-    }
-
-    return constructResponse(posts, 200, request);
-}
-
-async function handlePost(request) {
-    let contentType = request.headers.get("content-type");
-    if (!contentType || !contentType.includes("application/json")) {
-        // bad request
-        return constructResponse("Not JSON!", 400, request);
-    }
-
-    let newPost = await request.json();
-    if (
-        !newPost.hasOwnProperty("title") ||
-        !newPost.hasOwnProperty("username") ||
-        !newPost.hasOwnProperty("content")
-    ) {
-        // invalid format
-
-        // unprocessable entity
-        return constructResponse(
-            "Wrong post format! Must include title, username, and content",
-            422,
-            request
-        );
-    }
-
-    // this will be the initial response that will contain the cookie, and built upon at the end
-    let authorization;
-
-    let users = JSON.parse(await MY_KV.get("users"));
-    if (users.includes(newPost.username)) {
-        // existing user
-
-        authorization = await fetch(`${AUTH_SERVER}/verify`, {
-            headers: request.headers,
-        });
-
-        if (authorization.status === 498 || authorization.status === 401) {
-            // unauthorized
-            return constructResponse("Unauthorized", 401, request);
-        }
-
-        if (!authorization.ok) {
-            // internal server error
-            return constructResponse(
-                `Couldn't authenticate existing user, auth ${authorization.status}`,
-                500,
-                request
-            );
+            try {
+                return await handler(event.request);
+            } catch (err) {
+                throw err;
+                return {
+                    message: `Worker error`,
+                    status: 500,
+                };
+            }
+        } else {
+            return {
+                message: `Unimplemented method ${method} on ${endpoint}`,
+                status: 501,
+            };
         }
     } else {
-        // new user
-
-        authorization = await fetch(`${AUTH_SERVER}/auth/${newPost.username}`);
-
-        if (!authorization.ok) {
-            // something went wrong with the authentication server
-
-            // internal server error
-            return constructResponse(
-                `Couldn't authenticate new user, auth ${authorization.status}`,
-                500,
-                request
-            );
-        }
-
-        // add the user to our list
-        users.push(newPost.username);
-        await MY_KV.put("users", JSON.stringify(users));
+        return {
+            message: `Resource ${endpoint} Not Found`,
+            status: 404,
+        };
     }
+}
 
-    // current timestamp will serve as the key (this also allows us to retrieve posts in order!)
-    let ts = +new Date();
-    let key = "post:" + ts;
+async function handleFetch(event) {
+    let result = await router(event, {
+        "/posts": {
+            GET: async request => {
+                // the storage also includes other information, prefix limits us to posts only
+                let postList = await SOCIAL_MEDIA_KV.list({ prefix: "post:" });
 
-    await MY_KV.put(key, JSON.stringify(newPost));
+                let posts = [];
+                for (key of postList.keys) {
+                    // parsing every single one is quite inefficient
+                    // can technically treat it as a string always, and append commas and such...
+                    // but this is much easier to understand as code
+                    posts.push(JSON.parse(await SOCIAL_MEDIA_KV.get(key.name)));
+                }
 
-    // created
-    return new Response(JSON.stringify({ message: "Created" }), {
+                return {
+                    message: posts,
+                    status: 200,
+                };
+            },
+            POST: async request => {
+                let contentType = request.headers.get("content-type");
+                if (!contentType || !contentType.includes("application/json")) {
+                    // bad request
+                    return {
+                        message: "Data must be in JSON format",
+                        status: 400,
+                    };
+                }
+
+                let newPost = await request.json();
+                if (
+                    !newPost.hasOwnProperty("title") ||
+                    !newPost.hasOwnProperty("username") ||
+                    !newPost.hasOwnProperty("content")
+                ) {
+                    // invalid format
+
+                    // unprocessable entity
+                    return {
+                        message: "Wrong post format, must include: title, username, content",
+                        status: 422,
+                    };
+                }
+
+                // this will be the initial response that will contain the cookie, and built upon at the end
+                let authorization;
+
+                let users = JSON.parse(await SOCIAL_MEDIA_KV.get("users"));
+                if (users.includes(newPost.username)) {
+                    // existing user
+
+                    authorization = await fetch(`${AUTH_SERVER}/verify`, {
+                        headers: request.headers,
+                    });
+
+                    if (authorization.status === 498 || authorization.status === 401) {
+                        // unauthorized
+                        return {
+                            message: "Unauthorized",
+                            status: 401,
+                        };
+                    }
+
+                    if (!authorization.ok) {
+                        // internal server error
+                        return {
+                            message: `Couldn't authenticate existing user, auth ${authorization.status}`,
+                            status: 500,
+                        };
+                    }
+                } else {
+                    // new user
+
+                    authorization = await fetch(`${AUTH_SERVER}/auth/${newPost.username}`);
+
+                    console.log("during authorization");
+
+                    if (!authorization.ok) {
+                        // something went wrong with the authentication server
+
+                        // internal server error
+                        return {
+                            message: `Couldn't authenticate new user, auth ${authorization.status}`,
+                            status: 500,
+                        };
+                    }
+
+                    // add the user to our list
+                    users.push(newPost.username);
+                    await SOCIAL_MEDIA_KV.put("users", JSON.stringify(users));
+                }
+
+                // current timestamp will serve as the key (this also allows us to retrieve posts in order!)
+                let ts = +new Date();
+                let key = `post:${ts}`;
+
+                await SOCIAL_MEDIA_KV.put(key, JSON.stringify(newPost));
+
+                // created
+                return {
+                    message: "Post created",
+                    status: 201,
+                    meta: authorization,
+                };
+            },
+        },
+    });
+
+    return new Response(JSON.stringify({ message: result.message }), {
         headers: {
             "content-type": "application/json",
-            "Access-Control-Allow-Origin": request.headers.get("origin"),
+            "Access-Control-Allow-Origin": event.request.headers.get("origin"),
         },
-        ...authorization,
-        status: 201,
+        ...result.meta,
+        status: result.status,
         credentials: "include",
     });
 }
 
 addEventListener("fetch", event => {
-    let method = event.request.method.toUpperCase();
-
-    if (method === "GET") {
-        event.respondWith(handleGet(event.request));
-    } else if (method === "POST") {
-        event.respondWith(handlePost(event.request));
-    } else if (method === "OPTIONS") {
+    if (event.request.method.toUpperCase() === "OPTIONS") {
         // for CORS preflight
         event.respondWith(
             new Response(null, {
@@ -137,16 +164,7 @@ addEventListener("fetch", event => {
                 credentials: "include",
             })
         );
-    } else {
-        // not a handled type
-        event.respondWith(
-            new Response("Only GET and POST are supported", {
-                status: 501, // not implemented
-                headers: {
-                    "Access-Control-Allow-Origin": event.request.headers.get("origin"),
-                },
-                credentials: "include",
-            })
-        );
     }
+
+    event.respondWith(handleFetch(event));
 });
